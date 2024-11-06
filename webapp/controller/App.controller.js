22535309b031428693767e8bc5d591c22f7f880f
Gustavo Ovelar <gusove@gmail.comr>
2024-11-06 20:22:37 +0000

    First commit on Development

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("logalitrain.invoices.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"logalitrain/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
